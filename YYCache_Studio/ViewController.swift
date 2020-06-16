1ee0e9059baed69b6d6d72e7a5b7dbac038d159d
Aaron <835087646@qq.com>
2020-06-16 22:12:25 +0800

    Initial Commit

---
+++ YYCache_Studio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YYCache_Studio
+//
+//  Created by Aaron_Zhang on 2020/6/16.
+//  Copyright © 2020 Aaron_Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ YYCache_StudioTests/YYCache_StudioTests.swift
@@ -0,0 +1,34 @@
+//
+//  YYCache_StudioTests.swift
+//  YYCache_StudioTests
+//
+//  Created by Aaron_Zhang on 2020/6/16.
+//  Copyright © 2020 Aaron_Zhang. All rights reserved.
+//
+
+import XCTest
+@testable import YYCache_Studio
+
+class YYCache_StudioTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
