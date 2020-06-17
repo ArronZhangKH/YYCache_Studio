8342674614c4d67364d4ee169d0e092cd6bd659f
Aaron <835087646@qq.com>
2020-06-17 10:30:36 +0800

    init project

---
+++ Podfile
@@ -0,0 +1,9 @@
+
+
+platform :ios, '9.0'
+
+target 'YYCache_Studio' do
+
+	pod 'YYCache'
+
+end
---
+++ Pods/Target Support Files/Pods-YYCache_Studio/Pods-YYCache_Studio-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YYCache_Studio : NSObject
+@end
+@implementation PodsDummy_Pods_YYCache_Studio
+@end
